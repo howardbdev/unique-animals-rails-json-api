9e53f1a9596e9d6e45621ba4d34aa45cf417568b
howardbdev <howarddevennish@gmail.com>
2019-12-19 15:34:07 -0500

    allow all origins and update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+# Animal.create(
+#   {
+#     name: "Venezuelan Poodle Moth",
+#     fake_scientific_name: "mothus lookalikagoatus",
+#     description: "looks like a miniature flying goat",
+#     image_url: "https://www.snopes.com/tachyon/images/photos/animals/graphics/vpm.jpg?resize=500,380"
+#   }
+# )
+# Animal.create(
+#   {
+#     name: "Emperor Tamarin",
+#     fake_scientific_name: "monkeius mustacheus",
+#     description: "a very regal looking monkey",
+#     image_url: "https://upload.wikimedia.org/wikipedia/commons/5/55/Tamarin_portrait.JPG"
+#   }
+# )
 Animal.create(
   {
-    name: "Venezuelan Poodle Moth",
-    fake_scientific_name: "mothus lookalikagoatus",
-    description: "looks like a miniature flying goat",
-    image_url: "https://www.snopes.com/tachyon/images/photos/animals/graphics/vpm.jpg?resize=500,380"
-  }
-)
-Animal.create(
-  {
-    name: "Emperor Tamarin",
-    fake_scientific_name: "monkeius mustacheus",
-    description: "a very regal looking monkey",
-    image_url: "https://upload.wikimedia.org/wikipedia/commons/5/55/Tamarin_portrait.JPG"
+    name: "Manis",
+    fake_scientific_name: "lookalika anteaterfish",
+    description: "looks like an anteater with scales",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/3/30/Scaly_ant_eater_by_Dushy_Ranetunge_2.jpg"
   }
 )
